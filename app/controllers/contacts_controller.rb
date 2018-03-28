c338aa1e623f39b20d32e6140abb2abe511c837d
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-03-28 16:19:26 +0900

    added controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,18 @@
 class ContactsController < ApplicationController
+before_action :set_contact, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+  end
 
   def new
+    @contact = Contact.new
+  end
 
+  def edit
   end
 
   def create
@@ -16,6 +27,26 @@ class ContactsController < ApplicationController
         format.html { render :new }
         format.json { render json: @contact.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @contact.update(contact_params)
+        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
+        format.json { render :show, status: :ok, location: @contact }
+      else
+        format.html { render :edit }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @contact.destroy
+    respond_to do |format|
+      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
