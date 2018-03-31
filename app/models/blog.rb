17debbe7544185a6a91a44ac396c7d4204bad9a4
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-03-31 18:42:46 +0900

    モデルにImageUploader紐付け

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,5 @@ class Blog < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true,
                       length: { maximum: 140 }
+  mount_uploader :image, ImageUploader
 end
